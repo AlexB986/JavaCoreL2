1514ca593e6b7ce8d7364d00a548600a55aa87b8
AlexB986 <54481725+AlexB986@users.noreply.github.com>
2023-02-13 18:01:40 +0500

    Revert "Revert "Revert "Jcl3.0"""

---
+++ src/transport/Bus.java
@@ -0,0 +1,9 @@
+package transport;
+
+public class Bus  extends Transport{
+
+    public Bus(String brand, String model, String color, int maxSpeed, String assemblyCountry, int year) {
+        super(brand, model, color, maxSpeed, assemblyCountry, year);
+    }
+}
+
